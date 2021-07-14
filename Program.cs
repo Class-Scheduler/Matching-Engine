3f25a34b8312482022a39d886027aadbc8c336e0
Digby Emmett <2014902+DigbyEmmett@users.noreply.github.com>
2021-07-14 16:54:56 +0800

    Initialised C# Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace class_scheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
