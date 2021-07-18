db78aa0faa90495d990482cf0b4da3eb625a1423
jcluppnow <55492646+jcluppnow@users.noreply.github.com>
2021-07-18 13:51:49 +0800

    Tests - Created Test Unit class to test the parsing and output of the Unit model.

---
+++ tests/TestUnit.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Class_Scheduler.Tests.ParseTests
+{
+    [TestClass]
+    public class TestUnit
+    {
+        [TestMethod]
+        public void TestUnitOutput()
+        {
+
+        }
+    }
+}
